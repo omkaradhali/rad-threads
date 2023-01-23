ce496907a1caa582104d6e1a5ce9c210ce1f66f8
Omkar Adhali <omkar.a2989@gmail.com>
2023-01-23 15:08:24 -0500

    Adding bsaic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import Home from "./routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop =() => {
+  return (
+    <div>
+      <h1>I am shop component</h1>
+    </div>
+  )
+}
 
 const  App = () => {
 
   return (
-    <Home />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
 }
 
